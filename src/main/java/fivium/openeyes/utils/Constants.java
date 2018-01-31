8bdf214e3126ea4df7548b026beb0724ef1a64f9
ttrimbonias <t.trimbonias@gmail.com>
2018-01-31 12:06:41 +1100

    add new queries and cut down dao

--- src/main/java/fivium/openeyes/utils/Constants.java
+++ src/main/java/fivium/openeyes/utils/Constants.java
@@ -17,12 +17,50 @@ public final class Constants {
 		
 		QUERY_MAP.put("GetAllPatients", GET_ALL_PATIENTS_QUERY);
 		QUERY_MAP.put("GetPatientByID", GET_PATIENT_BY_ID);
+		QUERY_MAP.put("GetTotalInjectionsByDrug", GET_TOTAL_INJECTIONS_BY_DRUG);
+		QUERY_MAP.put("GetTotalInjectionsByDrugAndPatient", GET_TOTAL_INJECTIONS_BY_DRUG_AND_PATIENT);
 		
 		return QUERY_MAP;
 	}
 	
 	
 	private static final String GET_ALL_PATIENTS_QUERY = "Select * from patient";
+	
 	private static final String GET_PATIENT_BY_ID = "Select * from patient where id = ?";
+	
+	private static final String GET_TOTAL_INJECTIONS_BY_DRUG = "SELECT " +
+			"  treatment.name drug " +
+			", COUNT(*) total_injections " +
+			"FROM ( " +
+			"SELECT * " +
+			"FROM et_ophtrintravitinjection_treatment et_treatment " +
+			") i " +
+			"JOIN ophtrintravitinjection_treatment_drug treatment " +
+			"  ON (treatment.id = i.left_drug_id OR treatment.id = i.right_drug_id) " +
+			"GROUP BY " +
+			"  treatment.name; ";
+					
+	private static final String GET_TOTAL_INJECTIONS_BY_DRUG_AND_PATIENT = "SELECT " +
+			"  CONCAT(c.first_name, ' ', c.last_name) patient_name " +
+			", treatment.name medication " +
+			", COUNT(*) total_injections " +
+			"FROM ( " +
+			"SELECT * " +
+			"FROM et_ophtrintravitinjection_treatment et_treatment " +
+			") i " +
+			"JOIN ophtrintravitinjection_treatment_drug treatment " +
+			"  ON (treatment.id = i.left_drug_id OR treatment.id = i.right_drug_id) " +
+			"JOIN event e " +
+			"  ON e.id = i.event_id " +
+			"JOIN episode ep " +
+			"  ON ep.id = e.episode_id " +
+			"JOIN patient p " +
+			"  ON p.id = ep.patient_id " +
+			"JOIN contact c " +
+			"  ON c.id = p.contact_id " +
+			"GROUP BY " +
+			"  p.id " +
+			", treatment.id ";
+					
 	
 }
--- src/main/java/fivium/openeyes/utils/DAO.java
+++ src/main/java/fivium/openeyes/utils/DAO.java
@@ -9,9 +9,7 @@ import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -80,53 +78,5 @@ public class DAO {
 		
 	}
 
-	public static void executeSingleSQLStatementInBulk(String sql, Map<String, List<Object>> dataToBeStored) {
-		logger.info("Entering executeBulkStatements...");
-		try {
-			Connection connection = getConnection();
-			PreparedStatement statement = connection.prepareStatement(sql);
-			Set<String> dataSet = dataToBeStored.keySet();
-			for (String x : dataSet) {
-				List<Object> aList = dataToBeStored.get(x);
-				for (int i = 0; i < aList.size(); i++) {
-					statement.setString(i + 1, aList.get(i).toString());
-				}
-				statement.addBatch();
-			}
-			statement.executeBatch(); // Execute every 1000 items.
-		} catch (ClassNotFoundException | SQLException e) {
-			logger.error("ClassNotFoundException | SQLException occured storing the data into the database... " + e);
-		} catch (Exception e) {
-			logger.error("An Exception occured storing the data into the database... " + e);
-		}
-	}
 
-	@Deprecated
-	/**
-	 * Deprecated. - Use executeFetchStatement() or executeMutateStatement() instead.
-	 */
-	public static Collection<Map<String, String>> executeStatement(String sql, Object[] args)
-			throws SQLException, ClassNotFoundException {
-		Collection<Map<String, String>> returnData = new HashSet<Map<String, String>>();
-		PreparedStatement preparedStatement = getConnection().prepareStatement(sql);
-		// insert args
-		if (args != null) {
-			for (int i = 0; i < args.length; i++) {
-				preparedStatement.setObject(i + 1, args[i]);
-			}
-		}
-
-		ResultSet resultSet = preparedStatement.executeQuery();
-		ResultSetMetaData md = resultSet.getMetaData();
-		int columns = md.getColumnCount();
-		while (resultSet.next()) {
-			HashMap<String, String> row = new HashMap<String, String>(columns);
-			for (int i = 1; i <= columns; ++i) {
-				row.put(md.getColumnName(i), String.valueOf(resultSet.getObject(i)));
-			}
-			returnData.add(row);
-		}
-		return returnData;
-	}
-	
 }
